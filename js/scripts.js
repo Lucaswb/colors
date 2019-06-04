c5a00d2b0cd26be644d9a9392f73a756f0e95dbb
Erik Irgens and Lucas Blasdel <erik@>
2019-06-04 10:38:31 -0700

    Finish colors css and js.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  // $("button#green").click(function() {
+  //   $("body").removeClass();
+  //   $("body").addClass("green-background");
+  // });
+  //
+  // $("button#yellow").click(function() {
+  //   $("body").removeClass();
+  //   $("body").addClass("yellow-background");
+  // });
+  //
+  // $("button#red").click(function() {
+  //   $("body").removeClass();
+  //   $("body").addClass("red-background");
+  // });
+  $("button#green").click(function() {
+  $("body").css("background-color", "green");
+  });
+  $("button#yellow").click(function() {
+  $("body").css("background-color", "yellow");
+  });
+  $("button#red").click(function() {
+  $("body").css("background-color", "red");
+  });
+});
